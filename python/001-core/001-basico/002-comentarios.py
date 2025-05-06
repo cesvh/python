6f4608a8dac81c757eb8e8d94ea088dbf9442c17
cesvh <cesar.vega.hdez@gmail.com>
2025-05-06 00:51:52 -0600

    feat: python Comentarios

---
+++ python/001-core/001-basico/002-comentarios.py
@@ -0,0 +1,12 @@
+# Soy un comentario de una sola linea
+
+""" Soy un ejemplo de comentario docstrings"""
+print("Comentarios en python") # Otro comentario
+
+"""
+Soy un bloque de comentarios
+De varias lineas
+En python
+
+Este tipo de comentarios se suele usar mucho en la documentación
+"""
