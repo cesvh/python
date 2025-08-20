d5162f96320cfe6875920189ff8bb2222ce10127
cesvh <cesar.vega.hdez@gmail.com>
2025-08-20 00:37:57 -0600

    fix: install python, python pip version

--- python/001-core/001-basico/000-install-python.sh
+++ python/001-core/001-basico/000-install-python.sh
@@ -1,9 +1,10 @@
 PS C:\Users\cesvh>
 PS C:\Users\cesvh> python --version
-Python 3.13.3
+Python 3.13.7
+PS C:\Users\cesvh>
 PS C:\Users\cesvh>
 PS C:\Users\cesvh> pip --version
-pip 25.0.1 from C:\Users\cesvh\AppData\Local\Programs\Python\Python313\Lib\site-packages\pip (python 3.13)
+pip 25.2 from C:\Program Files\Python313\Lib\site-packages\pip (python 3.13)
 PS C:\Users\cesvh>
 PS C:\Users\cesvh>
 PS C:\Users\cesvh>
