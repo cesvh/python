a12fb3a9ba6ffa34467cbd6ad2dc251e9929b869
cesvh <cesar.vega.hdez@gmail.com>
2025-08-20 00:51:40 -0600

    fix: anteponer una r (raw string) al bloque de comillas triples: r""" ... """

--- python/001-core/001-basico/001-input-output.py
+++ python/001-core/001-basico/001-input-output.py
@@ -1,7 +1,7 @@
 user_name = input("Inserta tu nombre: ")
 print("Username: ", user_name)
 
-"""
+r"""
 PS C:\Users\cesvh\dev\github\python\python\001-core\001-basico>
 PS C:\Users\cesvh\dev\github\python\python\001-core\001-basico>
 PS C:\Users\cesvh\dev\github\python\python\001-core\001-basico> python .\001-input-output.py
